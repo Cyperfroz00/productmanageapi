a4b1db3c85a994acfddd678d8b72766c638670bc
Cyperfroz00 <pen_ponlok@pp.bbu.edu.kh>
2023-08-29 22:32:42 +0700

    4th update auth with dumyapi -update Local Storage

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:localstorage/localstorage.dart';
 import '../auth/constants/constants.dart';
 import '../auth/model/login_response.dart';
+
 class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
 
@@ -16,18 +17,31 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.indigo,
-        title: Text("Home Screen" ,style: TextStyle(color: Colors.white),),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(15),
-        child: Center(
-          child: Column(
-            children: [
-              Text("Login Success"),
-              Text("${storage.getItem(Constants.user_name_key)}")
-            ],
-          ),
+        title: Text(
+          "Home Screen",
+          style: TextStyle(color: Colors.white),
         ),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            child: GridView.builder(
+              itemCount: 12,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 4.0,
+                  mainAxisSpacing: 4.0),
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    color: Colors.pink[300],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
